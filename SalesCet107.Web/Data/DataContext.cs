ae901a148c5ea58f0de8af64d2c0b58f5531cd93
EdgarCastro <cafucastro12@gmail.com>
2026-07-07 15:52:46 +0100

    Create database and coutries talbe

---
+++ SalesCet107.Web/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesCet107.Web.Data.Entities;
+
+namespace SalesCet107.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Country> Countries { get; set; }
+    }
+}
---
+++ SalesCet107.Web/Data/Entities/Country.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SalesCet107.Web.Data.Entities
+{
+    public class Country
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name="País")]
+        [MaxLength(50, ErrorMessage = "O campo {0} deve ter no maximo {1}!")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string Name { get; set; }
+    }
+}
